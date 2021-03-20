bbb42efa0cf7de3d92a52a18bdacd53c983461a7
SchmidtPeti <schmidt.peti32@gmail.com>
2021-03-20 18:22:07 +0100

    testing out the website

--- Main.js
+++ Main.js
@@ -4,6 +4,6 @@ const ImprovedBubble = () => {
     let element = document.getElementById('ImprovedSortArray').value;
     let elements = element.split(`.`).map(x=>+x)
     ImprovedBubbleSort(elements);
-    console.log(elements);
+    console.log(element);
 }
 document.getElementById("ImprovedBubble").addEventListener("click", ImprovedBubble);
