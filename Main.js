ae1e7274eb2a205cd253b04c0729cb2c2be0a666
SchmidtPeti <schmidt.peti32@gmail.com>
2021-03-20 18:30:52 +0100

    testing out the website

--- Main.js
+++ Main.js
@@ -1,9 +1,16 @@
 import ImprovedBubbleSort from "./ImprovedBubbleSort/ImprovedBubbleSort.js";
+import { MaxSelectionSort } from "./SelectionSort/SelectionSort.js";
 
 const ImprovedBubble = () => {
     let element = document.getElementById('ImprovedSortArray').value;
     let elements = element.split(`,`).map(x=>+x)
     ImprovedBubbleSort(elements);
-    console.log(element);
+    
+}
+const MaxSelection = () => {
+    let element = document.getElementById('MaxSelectionArray').value;
+    let elements = element.split(`,`).map(x=>+x)
+    MaxSelectionSort(elements)
 }
 document.getElementById("ImprovedBubble").addEventListener("click", ImprovedBubble);
+document.getElementById("MaxSelection").addEventListener("click", MaxSelection);
