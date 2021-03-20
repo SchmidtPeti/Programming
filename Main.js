847283a519cfd83feffc364f53b94edea4c6fecd
SchmidtPeti <schmidt.peti32@gmail.com>
2021-03-20 18:10:35 +0100

    testing out the website

--- Main.js
+++ Main.js
@@ -3,5 +3,6 @@ import ImprovedBubbleSort from "./ImprovedBubbleSort/ImprovedBubbleSort.js";
 const ImprovedBubble = () => {
     let elements = document.getElementById('ImprovedSortArray').value.split('.').map(x=>+x);
     ImprovedBubbleSort(elements);
+    console.log("Malac");
 }
 document.getElementById("ImprovedBubble").addEventListener("click", ImprovedBubble);
