ffee91a4519dc4c38a1dfb5276cc00706d115dd8
SchmidtPeti <schmidt.peti32@gmail.com>
2021-03-20 18:23:13 +0100

    testing out the website

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@ import ImprovedBubbleSort from "./ImprovedBubbleSort/ImprovedBubbleSort.js";
 
 const ImprovedBubble = () => {
     let element = document.getElementById('ImprovedSortArray').value;
-    let elements = element.split(`.`).map(x=>+x)
+    let elements = element.split(`,`).map(x=>+x)
     ImprovedBubbleSort(elements);
     console.log(element);
 }
