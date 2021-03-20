927a3658b11cbd4a190d0b8744b58f9aaff4f9cf
SchmidtPeti <schmidt.peti32@gmail.com>
2021-03-20 17:55:42 +0100

    main.js

--- Main.js
+++ Main.js
@@ -1,6 +1,6 @@
-import { MaxSelectionSort } from "./SelectionSort/SelectionSort.js";
+import {ImprovedBubbleSort } from "./ImprovedBubbleSort/ImprovedBubbleSort.js";
 
-let array = [2,3,4,4];
-MaxSelectionSort(array);
-console.log(array);
-console.log("Im here");
+const ImprovedBubble = () => {
+    let elements = document.getElementById('ImprovedSortArray').value.split('.').map(x=>+x);
+    ImprovedBubbleSort(elements);
+}
